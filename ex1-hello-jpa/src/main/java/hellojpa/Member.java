1605bf0e98142247f48d91c54f0e8154dfc1bd56
leechanhoe <54972898+leechanhoe@users.noreply.github.com>
2023-01-20 14:21:29 +0900

    34~37강 23.1.20

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -3,9 +3,7 @@ package hellojpa;
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @Entity
 public class Member extends BaseEntity{
@@ -18,6 +16,35 @@ public class Member extends BaseEntity{
 
     @Column(name = "USERNAME")
     private String username;
+
+//    @Embedded
+//    private Period workPeriod;
+//
+    @Embedded
+    private Address homeAddress;
+
+//    @ElementCollection
+//    @CollectionTable(name = "FAVORITE_FOOD", joinColumns =
+//        @JoinColumn(name = "MEMBER_ID"))
+//    @Column(name = "FOOD_NAME")
+//    private Set<String> favoriteFoods = new HashSet<>();
+
+//    @ElementCollection
+//    @CollectionTable(name = "ADDRESS", joinColumns =
+//    @JoinColumn(name = "MEMBER_ID"))
+//    private List<Address> addressHistory = new ArrayList<>();
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "MEMBER_ID")
+    private List<AddressEntity> addressHistory = new ArrayList<>();
+//    @Embedded
+//    @AttributeOverrides({
+//            @AttributeOverride(name = "city", column = @Column(name = "WORK_CITY")),
+//            @AttributeOverride(name = "street", column = @Column(name = "WORK_STREET")),
+//            @AttributeOverride(name = "zipcode", column = @Column(name = "WORK_ZIPCODE"))
+//
+//    })
+//    private Address workAddress;
 
 //    @Column(name = "TEAM_ID")
 //    private Long teamId;
